3b865341eb8f0fbdae82f8202f3fd4e66b233146
vvkhil <MVK68-LBJ@yandex.by>
2022-10-07 01:07:29 +0300

    Add model

---
+++ app/src/main/java/com/example/timer/model/Sequence.kt
@@ -0,0 +1,26 @@
+package com.example.timer.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "sequence_table")
+class Sequence(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    var title: String,
+    var color: Int,
+    var warmUp: Int,
+    var workout: Int,
+    var rest: Int,
+    var cycles: Int,
+    var cooldown: Int,
+    var voice_on: Boolean
+): Parcelable{
+    fun totalDuration() = warmUp +
+            workout * cycles +
+            rest * (cycles - 1) +
+            cooldown
+}
